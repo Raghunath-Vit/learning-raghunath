51468042e636eca724df323427579ee34edbb445
Raghunath-Vit <raghunathsinghmannu1@gmail.com>
2024-08-09 00:29:04 +0530

    Material UI pdf task completed

---
+++ muiPdfAssign/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MaterialUi from './MatrialUi';
+import Topograph from './Topograph';
+import Gridlay from './Gridlay';
+import ThemedApp from './ThemedApp';
+function App() {
+
+return (
+<div>
+<MaterialUi/>
+<Topograph/>
+<Gridlay/>
+<ThemedApp/>
+ReactDOM.render(
+<React.StrictMode>
+<ThemedApp />
+</React.StrictMode>,
+document.getElementById('root')
+);
+</div>
+);
+}
+export default App;
+
